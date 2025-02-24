3998d10551e6fa8ab6105a8f23daf68f446d6085
Omlalwani <lalwaniom614@gmail.com>
2025-02-24 10:57:16 +0530

    Here, We are Starting DSA from scratch with the use of "Java Language". Goal is to complete full DSA in 6 Months

--- src/Maths/countDigits.java
+++ src/Maths/countDigits.java
@@ -2,5 +2,17 @@ package Maths;
 
 public class countDigits
 {
+    public static void main(String[] args)
+    {
+        int num = 54321;
+        int count = 0;
+        while(num > 0)
+        {
+            int temp = num % 10;
+            count++;
+            num /= 10;
+        }
 
+        System.out.println("The count of numbers is : " + count);
+    }
 }
