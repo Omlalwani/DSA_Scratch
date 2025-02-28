5403d75e1bd1f9b445df19dd61438cd3ffe02838
Omlalwani <Omlalwani@users.noreply.github.com.>
2025-02-28 11:27:56 +0530

    The program to calculate gcd, with all possible functions

--- src/Maths/GCD.java
+++ src/Maths/GCD.java
@@ -1,4 +1,58 @@
 package Maths;
 
-public class GCD {
+public class GCD
+{
+
+    //Logic - 1. It will loop until the biggest number, as we know it will go till min number from both.
+    static int greatorDivisor(int a, int b)
+    {
+        int checkGreator = 0;
+        for(int i=1; i<= b; i++)
+        {
+            if((a % i == 0) && (b % i == 0))
+                checkGreator = i;
+        }
+
+        return checkGreator;
+    }
+
+
+    //Logic - 2. It will loop until the min number from both using while loop - TC :- O(min(a,b))
+    static int greatestDivisor(int a, int b)
+    {
+        int result = Math.min(a, b);
+        while(result > 0)
+        {
+            if ((a % result == 0) && (b % result == 0))
+                break;
+            result--;
+        }
+        return result;
+    }
+
+    //Logic - 3. Using Euclidean Algorithm....
+
+    static int eculideanGCD(int a, int b)
+    {
+        while(a != b)
+        {
+            if(a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a = 12, b = 15;
+        int print = greatorDivisor(a, b);
+        System.out.println(print);
+        System.out.println("----------------");
+        int print2 = greatestDivisor(a, b);
+        System.out.println(print2);
+        System.out.println("----------------");
+        int eculideanAlgo = eculideanGCD(a, b);
+        System.out.println(eculideanAlgo);
+    }
 }
--- src/Maths/trailingZerosInFactorial.java
+++ src/Maths/trailingZerosInFactorial.java
@@ -12,9 +12,21 @@ public class trailingZerosInFactorial
         return res;
     }
 
+    // AN efficient way for handling big factorial's, as below if you write n > 16 you will get garbage values
+    // to handle that this func is being created.
+
+    static int countZeros(int n)
+    {
+        int result = 0;
+        for(int i=5; i<= n; i = i * 5)
+            result = result + (n/i);
+
+        return result;
+    }
+
     public static void main(String[] args)
     {
-        int n = 16;
+        /*int n = 16;
         long result = factorial(n); //Use bigInteger for the longer range of the result...
         long temp = result;
         int count = 0;
@@ -26,7 +38,14 @@ public class trailingZerosInFactorial
                 count++;
             temp /= 10;
         }
+        System.out.println("The count of 0's in " + result + " is : " + count);*/
 
-        System.out.println("The count of 0's in " + result + " is : " + count);
+
+        //------Used efficient way----
+        int j = 250;
+        int res = countZeros(j);
+
+        System.out.println("The count of 0's in " + j + " is : " + res);
+
     }
 }
