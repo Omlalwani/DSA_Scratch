4bc57180b11ef6b926ed66c3d62536b308ff4157
Omlalwani <Omlalwani@users.noreply.github.com.>
2025-03-04 11:00:40 +0530

    The programs shows the all divisors of the current number

---
+++ src/Maths/AllDivisorsOfNumber.java
@@ -0,0 +1,53 @@
+package Maths;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllDivisorsOfNumber
+{
+    static void divisorsUsingList(int n)
+    {
+        List<Integer> addNum = new ArrayList<>();
+        for(int i = 1; i<=n; i++)
+        {
+            if(n % i == 0)
+                addNum.add(i);
+        }
+        System.out.println(addNum);
+    }
+
+    static void printDivisors(int n)
+    {
+        for(int i=1; i*i<=n; i++) {
+            if (n % i == 0)
+            {
+                System.out.print(i + " ");
+                if (i != n / i)
+                    System.out.print(n / i + " ");
+            }
+        }
+    } //TC :- O(log n)
+
+    static void printDivisorsEfficient(int n)
+    {
+        int i;
+        for(i = 1; i*i<n; i++)
+            if(n % i == 0)
+                System.out.print(i + " ");
+
+        for( ;i>=1; i--)
+            if(n % i == 0)
+                System.out.print(n/i + " ");
+    }
+
+
+    public static void main(String[] args) {
+        int n = 30;
+        divisorsUsingList(n);
+        System.out.println("-----------------------");
+        printDivisors(n);
+        System.out.println();
+        System.out.println("-----------------------");
+        printDivisorsEfficient(n);
+    }
+}
